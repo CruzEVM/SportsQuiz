42971411e7d76a59a2e574d1972065072d55deba
Cruz Eduardo Valadez Meléndez <cruz.valadez19@tectijuana.edu.mx>
2023-03-20 18:17:41 -0700

    commit

--- app/src/main/java/com/example/sportsquiz/Welcome.java
+++ app/src/main/java/com/example/sportsquiz/Welcome.java
@@ -2,13 +2,40 @@ package com.example.sportsquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
+
+    Button comenzar;
+    Button instruccciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        comenzar=(Button) findViewById(R.id.comenzar);
+        comenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+
+        instruccciones=(Button) findViewById(R.id.Instrucciones);
+        instruccciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this, Instrucciones.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
