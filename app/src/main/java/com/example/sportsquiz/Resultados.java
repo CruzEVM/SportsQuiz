58139dfaa44ea950572c10f3958b97aea6a2ade2
Cruz Eduardo Valadez Meléndez <cruz.valadez19@tectijuana.edu.mx>
2023-04-07 12:57:10 -0700

    commit

---
+++ app/src/main/java/com/example/sportsquiz/Resultados.java
@@ -0,0 +1,67 @@
+package com.example.sportsquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class Resultados extends AppCompatActivity {
+
+    MaterialCardView home;
+    TextView correctt, wrongt, resultinfo, resultscore;
+    ImageView resultImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados);
+
+        home = findViewById(R.id.returnHome);
+        correctt = findViewById(R.id.correctScore);
+        wrongt = findViewById(R.id.wrongScore);
+        resultinfo = findViewById(R.id.resultInfo);
+        resultscore = findViewById(R.id.resultScore);
+        resultImage = findViewById(R.id.resultImage);
+
+        int correct = getIntent().getIntExtra("correct", 0);
+        int wrong = getIntent().getIntExtra("wrong", 0);
+        int score = correct * 10;
+
+        correctt.setText("" + correct);
+        wrongt.setText("" + wrong);
+        resultscore.setText("" + score);
+
+        if (correct >= 0 && correct <= 2) {
+            resultinfo.setText("You have to take the test again");
+            resultImage.setImageResource(R.drawable.basquelbol);
+        } else if (correct >= 3 && correct <= 5) {
+            resultinfo.setText("You have to try a little more");
+            resultImage.setImageResource(R.drawable.americano);
+        } else if (correct >= 6 && correct <= 8) {
+            resultinfo.setText("You are pretty good");
+            resultImage.setImageResource(R.drawable.f1);
+        } else {
+            resultinfo.setText("You are very good congratulations");
+            resultImage.setImageResource(R.drawable.futbol);
+        }
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Resultados.this, Menu.class));
+                finish();
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(Resultados.this, Menu.class));
+        finish();
+    }
+
+}
--- app/src/main/java/com/example/sportsquiz/Welcome.java
+++ app/src/main/java/com/example/sportsquiz/Welcome.java
@@ -2,10 +2,13 @@ package com.example.sportsquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class Welcome extends AppCompatActivity {
 
@@ -16,7 +19,7 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        comenzar=(Button) findViewById(R.id.comenzar);
+        comenzar = (Button) findViewById(R.id.comenzar);
         comenzar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,7 +28,7 @@ public class Welcome extends AppCompatActivity {
             }
         });
 
-        instruccciones=(Button) findViewById(R.id.Instrucciones);
+        instruccciones = (Button) findViewById(R.id.Instrucciones);
         instruccciones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,9 +36,25 @@ public class Welcome extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
-
-
-
+    @Override
+    public void onBackPressed() {
+        MaterialAlertDialogBuilder materialAlertDialogBuilder = new MaterialAlertDialogBuilder(Welcome.this);
+        materialAlertDialogBuilder.setTitle(R.string.app_name);
+        materialAlertDialogBuilder.setMessage("¿Seguro que quieres salir de la aplicación?");
+        materialAlertDialogBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        materialAlertDialogBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        materialAlertDialogBuilder.show();
     }
 }
