202036b43849eb733d9508c0549225c8c4a41320
Cruz Eduardo Valadez Meléndez <cruz.valadez19@tectijuana.edu.mx>
2023-05-16 17:21:19 -0700

    commit

--- app/src/main/java/com/example/sportsquiz/Instrucciones.java
+++ app/src/main/java/com/example/sportsquiz/Instrucciones.java
@@ -1,19 +1,17 @@
 package com.example.sportsquiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 public class Instrucciones extends AppCompatActivity {
 
-    Button atras;
+    Button atr;
     Button sig;
 
     @Override
@@ -23,15 +21,17 @@ public class Instrucciones extends AppCompatActivity {
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.americano));
-        atras=(Button) findViewById(R.id.after);
-        sig=(Button) findViewById(R.id.next);
+        atr=(Button) findViewById(R.id.atr1);
+        sig=(Button) findViewById(R.id.sig1);
 
-        atras.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Instrucciones.this, Welcome.class);
-                startActivity(intent);
-            }
+        atr.setOnClickListener(view -> {
+            Intent intent = new Intent(Instrucciones.this, Welcome.class);
+            startActivity(intent);
+        });
+
+        sig.setOnClickListener(view -> {
+            Intent intent = new Intent(Instrucciones.this, Ins1.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/example/sportsquiz/basquetbol.java
+++ app/src/main/java/com/example/sportsquiz/basquetbol.java
@@ -1,19 +1,14 @@
 package com.example.sportsquiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
@@ -23,6 +18,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -33,17 +29,14 @@ public class basquetbol extends AppCompatActivity {
     List<QuesitionsItem> quesitionsItems;
     int currentQuestions = 0;
     int correct = 0, wrong = 0;
-    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basquetbol);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(ContextCompat.getColor(this, R.color.basquetbol));
-        }
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.basquetbol));
         quiztext = findViewById(R.id.quizText);
         aans = findViewById(R.id.aanswer);
         bans = findViewById(R.id.banswer);
@@ -54,139 +47,115 @@ public class basquetbol extends AppCompatActivity {
         Collections.shuffle(quesitionsItems);
         setQuestionScreen(currentQuestions);
 
-        aans.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (quesitionsItems.get(currentQuestions).getAnswer1().equals(quesitionsItems.get(currentQuestions).getCorrect())) {
-                    correct++;
-                    aans.setBackgroundResource(R.color.green);
-                    aans.setTextColor(getResources().getColor(R.color.white));
-                } else {
-                    wrong++;
-                    aans.setBackgroundResource(R.color.red);
-                    aans.setTextColor(getResources().getColor(R.color.white));
-                }
+        aans.setOnClickListener(view -> {
+            if (quesitionsItems.get(currentQuestions).getAnswer1().equals(quesitionsItems.get(currentQuestions).getCorrect())) {
+                correct++;
+                aans.setBackgroundResource(R.color.green);
+                aans.setTextColor(getResources().getColor(R.color.white));
+            } else {
+                wrong++;
+                aans.setBackgroundResource(R.color.red);
+                aans.setTextColor(getResources().getColor(R.color.white));
+            }
 
-                if (currentQuestions < quesitionsItems.size()-1) {
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            currentQuestions++;
-                            setQuestionScreen(currentQuestions);
-                            aans.setBackgroundResource(R.color.white);
-                            aans.setTextColor(getResources().getColor(R.color.black));
-                        }
-                    }, 1000);
-                } else {
-                    Intent intent = new Intent(basquetbol.this, Resultados.class);
-                    intent.putExtra("Correctas", correct);
-                    intent.putExtra("Incorrectas", wrong);
-                    startActivity(intent);
-                    finish();
-                }
+            if (currentQuestions < quesitionsItems.size()-1) {
+                Handler handler = new Handler();
+                handler.postDelayed(() -> {
+                    currentQuestions++;
+                    setQuestionScreen(currentQuestions);
+                    aans.setBackgroundResource(R.color.white);
+                    aans.setTextColor(getResources().getColor(R.color.black));
+                }, 1000);
+            } else {
+                Intent intent = new Intent(basquetbol.this, Resultados.class);
+                intent.putExtra("Correctas", correct);
+                intent.putExtra("Incorrectas", wrong);
+                startActivity(intent);
+                finish();
             }
         });
 
-        bans.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (quesitionsItems.get(currentQuestions).getAnswer2().equals(quesitionsItems.get(currentQuestions).getCorrect())) {
-                    correct++;
-                    bans.setBackgroundResource(R.color.green);
-                    bans.setTextColor(getResources().getColor(R.color.white));
-                } else {
-                    wrong++;
-                    bans.setBackgroundResource(R.color.red);
-                    bans.setTextColor(getResources().getColor(R.color.white));
-                }
+        bans.setOnClickListener(view -> {
+            if (quesitionsItems.get(currentQuestions).getAnswer2().equals(quesitionsItems.get(currentQuestions).getCorrect())) {
+                correct++;
+                bans.setBackgroundResource(R.color.green);
+                bans.setTextColor(getResources().getColor(R.color.white));
+            } else {
+                wrong++;
+                bans.setBackgroundResource(R.color.red);
+                bans.setTextColor(getResources().getColor(R.color.white));
+            }
 
-                if (currentQuestions < quesitionsItems.size()-1) {
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            currentQuestions++;
-                            setQuestionScreen(currentQuestions);
-                            bans.setBackgroundResource(R.color.white);
-                            bans.setTextColor(getResources().getColor(R.color.black));
-                        }
-                    }, 1000);
-                } else {
-                    Intent intent = new Intent(basquetbol.this, Resultados.class);
-                    intent.putExtra("Correctas", correct);
-                    intent.putExtra("Incorrectas", wrong);
-                    startActivity(intent);
-                    finish();
-                }
+            if (currentQuestions < quesitionsItems.size()-1) {
+                Handler handler = new Handler();
+                handler.postDelayed(() -> {
+                    currentQuestions++;
+                    setQuestionScreen(currentQuestions);
+                    bans.setBackgroundResource(R.color.white);
+                    bans.setTextColor(getResources().getColor(R.color.black));
+                }, 1000);
+            } else {
+                Intent intent = new Intent(basquetbol.this, Resultados.class);
+                intent.putExtra("Correctas", correct);
+                intent.putExtra("Incorrectas", wrong);
+                startActivity(intent);
+                finish();
             }
         });
 
-        cans.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (quesitionsItems.get(currentQuestions).getAnswer3().equals(quesitionsItems.get(currentQuestions).getCorrect())) {
-                    correct++;
-                    cans.setBackgroundResource(R.color.green);
-                    cans.setTextColor(getResources().getColor(R.color.white));
-                } else {
-                    wrong++;
-                    cans.setBackgroundResource(R.color.red);
-                    cans.setTextColor(getResources().getColor(R.color.white));
-                }
+        cans.setOnClickListener(view -> {
+            if (quesitionsItems.get(currentQuestions).getAnswer3().equals(quesitionsItems.get(currentQuestions).getCorrect())) {
+                correct++;
+                cans.setBackgroundResource(R.color.green);
+                cans.setTextColor(getResources().getColor(R.color.white));
+            } else {
+                wrong++;
+                cans.setBackgroundResource(R.color.red);
+                cans.setTextColor(getResources().getColor(R.color.white));
+            }
 
-                if (currentQuestions < quesitionsItems.size()-1) {
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            currentQuestions++;
-                            setQuestionScreen(currentQuestions);
-                            cans.setBackgroundResource(R.color.white);
-                            cans.setTextColor(getResources().getColor(R.color.black));
-                        }
-                    }, 1000);
-                } else {
-                    Intent intent = new Intent(basquetbol.this, Resultados.class);
-                    intent.putExtra("Correctas", correct);
-                    intent.putExtra("Incorrectas", wrong);
-                    startActivity(intent);
-                    finish();
-                }
+            if (currentQuestions < quesitionsItems.size()-1) {
+                Handler handler = new Handler();
+                handler.postDelayed(() -> {
+                    currentQuestions++;
+                    setQuestionScreen(currentQuestions);
+                    cans.setBackgroundResource(R.color.white);
+                    cans.setTextColor(getResources().getColor(R.color.black));
+                }, 1000);
+            } else {
+                Intent intent = new Intent(basquetbol.this, Resultados.class);
+                intent.putExtra("Correctas", correct);
+                intent.putExtra("Incorrectas", wrong);
+                startActivity(intent);
+                finish();
             }
         });
 
-        dans.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (quesitionsItems.get(currentQuestions).getAnswer4().equals(quesitionsItems.get(currentQuestions).getCorrect())) {
-                    correct++;
-                    dans.setBackgroundResource(R.color.green);
-                    dans.setTextColor(getResources().getColor(R.color.white));
-                } else {
-                    wrong++;
-                    dans.setBackgroundResource(R.color.red);
-                    dans.setTextColor(getResources().getColor(R.color.white));
-                }
+        dans.setOnClickListener(view -> {
+            if (quesitionsItems.get(currentQuestions).getAnswer4().equals(quesitionsItems.get(currentQuestions).getCorrect())) {
+                correct++;
+                dans.setBackgroundResource(R.color.green);
+                dans.setTextColor(getResources().getColor(R.color.white));
+            } else {
+                wrong++;
+                dans.setBackgroundResource(R.color.red);
+                dans.setTextColor(getResources().getColor(R.color.white));
+            }
 
-                if (currentQuestions < quesitionsItems.size()-1) {
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            currentQuestions++;
-                            setQuestionScreen(currentQuestions);
-                            dans.setBackgroundResource(R.color.white);
-                            dans.setTextColor(getResources().getColor(R.color.black));
-                        }
-                    }, 1000);
-                } else {
-                    Intent intent = new Intent(basquetbol.this, Resultados.class);
-                    intent.putExtra("Correctas", correct);
-                    intent.putExtra("Incorrectas", wrong);
-                    startActivity(intent);
-                    finish();
-                }
+            if (currentQuestions < quesitionsItems.size()-1) {
+                Handler handler = new Handler();
+                handler.postDelayed(() -> {
+                    currentQuestions++;
+                    setQuestionScreen(currentQuestions);
+                    dans.setBackgroundResource(R.color.white);
+                    dans.setTextColor(getResources().getColor(R.color.black));
+                }, 1000);
+            } else {
+                Intent intent = new Intent(basquetbol.this, Resultados.class);
+                intent.putExtra("Correctas", correct);
+                intent.putExtra("Incorrectas", wrong);
+                startActivity(intent);
+                finish();
             }
         });
     }
@@ -200,7 +169,7 @@ public class basquetbol extends AppCompatActivity {
 
     private void loadAllQuestions() {
         quesitionsItems = new ArrayList<>();
-        String jsonquiz = loadJsonFromAsset("Basquetbol.json");
+        String jsonquiz = loadJsonFromAsset();
         try {
             JSONObject jsonObject = new JSONObject(jsonquiz);
             JSONArray questions = jsonObject.getJSONArray("Basquetbol");
@@ -221,15 +190,15 @@ public class basquetbol extends AppCompatActivity {
         }
     }
 
-    private String loadJsonFromAsset(String s) {
+    private String loadJsonFromAsset() {
         String json = "";
         try {
-            InputStream inputStream = getAssets().open(s);
+            InputStream inputStream = getAssets().open("Basquetbol.json");
             int size = inputStream.available();
             byte[] buffer = new byte[size];
             inputStream.read(buffer);
             inputStream.close();
-            json = new String(buffer, "UTF-8");
+            json = new String(buffer, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -241,18 +210,8 @@ public class basquetbol extends AppCompatActivity {
         MaterialAlertDialogBuilder materialAlertDialogBuilder = new MaterialAlertDialogBuilder(basquetbol.this);
         materialAlertDialogBuilder.setTitle(R.string.app_name);
         materialAlertDialogBuilder.setMessage("¿Seguro que quieres salir de la cuestionario?");
-        materialAlertDialogBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-        materialAlertDialogBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                finish();
-            }
-        });
+        materialAlertDialogBuilder.setNegativeButton(android.R.string.no, (dialogInterface, i) -> dialogInterface.dismiss());
+        materialAlertDialogBuilder.setPositiveButton(android.R.string.yes, (dialogInterface, i) -> finish());
         materialAlertDialogBuilder.show();
     }
 }
