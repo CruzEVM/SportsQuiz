42b47a4ada6aa0971392f165dd904145401cda97
Cruz Eduardo Valadez Meléndez <cruz.valadez19@tectijuana.edu.mx>
2023-05-16 17:38:16 -0700

    commit

---
+++ app/src/main/java/com/example/sportsquiz/in_Respuesta.java
@@ -0,0 +1,14 @@
+package com.example.sportsquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class in_Respuesta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_respuesta);
+    }
+}
---
+++ app/src/main/java/com/example/sportsquiz/in_resultados.java
@@ -0,0 +1,14 @@
+package com.example.sportsquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class in_resultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_resultados);
+    }
+}
